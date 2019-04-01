c395e1f9986ec95fcc261717bda6d00d5496d848
bferrell514 <bferrell514@gmail.com>
2019-04-01 00:41:50 -0400

    deleting used querySelector and e.target

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
-var btns = document.querySelectorAll('#book-list .delete');
+const list = document.querySelector('#book-list ul');
 
-Array.from(btns).forEach(function(btn){
-    btn.addEventListener('click',function(e){
-        
-        
+//deletes books from the page
+list.addEventListener('click', function(e){
+    if(e.target.className == 'delete') {
+        //grabs the parent above the li in the #book-list
         const li = e.target.parentElement;
-        //in order to remove the child you have to find the parent first
-        li.parentNode.removeChild(li);
         
-    });
-    
-});
-//connecting to the page banner links I created for "netninja"
-const link = document.querySelector('#page-banner a');
-
-//logging the link will not be going to the link its connected to and provide message in log
-link.addEventListener('click', function(e){
-  e.preventDefault();
-  console.log('navigation to ', e.target.textContent, 'was prevented');
-});
+        //removes the chilf to the parent node
+        list.parentNode.removeChild(li)
+    }
+})
