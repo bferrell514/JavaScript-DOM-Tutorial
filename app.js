0cbf66ebc864e46e2e53e1aed5e9eb23631ce90d
bferrell514 <bferrell514@gmail.com>
2019-04-01 13:26:28 -0400

    search bar filter in with books

--- app.js
+++ app.js
@@ -1,3 +1,44 @@
+const list = document.querySelector('#book-list ul');
+
+//deletes books from the page
+list.addEventListener('click', function(e){
+if(e.target.className == 'delete') {
+    //grabs the parent above the li in the #book-list
+    const li = e.target.parentElement;
+    
+    //removes the chilf to the parent node
+    list.removeChild(li);
+    }
+});
+
+//add book list
+//stop from reloading the page when you click add 
+//referencing the book
+const addForm = document.forms['add-book'];
+
+//add the event listener to the submit and prevent the default
+addForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    //The input to the given field
+    const value = addForm.querySelector('input[type ="text"]').value;
+
+    //created elements
+    const li = document.createElement('li');
+    const bookName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
+
+    //add content
+    deleteBtn.textContent = 'delete';
+    bookName.textContent = value;
+    
+    //apend span to document
+    //PS ORDER MATTERS
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+
+});
+
 //filter books
 const searchBar = document.forms['search-books'].querySelector('input');
 searchBar.addEventListener('keyup', (e) => {
@@ -14,20 +55,4 @@ searchBar.addEventListener('keyup', (e) => {
       book.style.display = 'none';
     }
   });
-});
-
-//tabbed content
-const tabs = document.querySelector('.tabs');
-const panels = document.querySelectorAll('.panel');
-tabs.addEventListener('click', (e) => {
-  if(e.target.tagName == 'LI'){
-    const targetPanel = document.querySelector(e.target.dataset.target);
-    Array.from(panels).forEach((panel) => {
-      if(panel == targetPanel){
-        panel.classList.add('active');
-      }else{
-        panel.classList.remove('active');
-      }
-    });
-  }
 });
