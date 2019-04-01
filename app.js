af98efabc65f45619fa84d774456b061637d81a9
bferrell514 <bferrell514@gmail.com>
2019-04-01 11:51:48 -0400

    appending books to end of list

--- app.js
+++ app.js
@@ -1,40 +1,34 @@
 const list = document.querySelector('#book-list ul');
 
-//deletes books from the page
-list.addEventListener('click', function(e){
-if(e.target.className == 'delete') {
-    //grabs the parent above the li in the #book-list
+// delete books
+list.addEventListener('click', (e) => {
+  if(e.target.className == 'delete'){
     const li = e.target.parentElement;
-    
-    //removes the chilf to the parent node
-    list.parentNode.removeChild(li);
-}
+    li.parentNode.removeChild(li);
+  }
 });
 
-//add book list
-//stop from reloading the page when you click add 
-//referencing the book
+// add books
 const addForm = document.forms['add-book'];
-
-//add the event listener to the submit and prevent the default
 addForm.addEventListener('submit', function(e){
-    e.preventDefault();
-    //The input to the given field
-    const value = addForm.querySelector('input[type="text"]');
+  e.preventDefault();
 
-//created elements
-const li = document.createElement('li');
-const bookName = document.createElement('span');
-const deleteBtn = document.createElement('span');
+  // create elements
+  const value = addForm.querySelector('input[type="text"]').value;
+  const li = document.createElement('li');
+  const bookName = document.createElement('span');
+  const deleteBtn = document.createElement('span');
 
-//add content
-deleteBtn.textContent = 'delete';
-bookName.textContent = value;
+  // add text content
+  bookName.textContent = value;
+  deleteBtn.textContent = 'delete';
 
-//apend span to document
-//PS ORDER MATTERS
-li.appendChild(bookName);
-li.appendChild(deleteBtn);
-list.appendChild(li);
+  // add classes
+  bookName.classList.add('name');
+  deleteBtn.classList.add('delete');
 
+  // append to DOM
+  li.appendChild(bookName);
+  li.appendChild(deleteBtn);
+  list.appendChild(li);
 });
