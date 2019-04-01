e069af25f31057155ca54f3315676450f715eb71
bferrell514 <bferrell514@gmail.com>
2019-04-01 13:07:00 -0400

    adding the tabbed content and using panels

--- app.js
+++ app.js
@@ -1,59 +1,33 @@
-const list = document.querySelector('#book-list ul');
-
-// delete books
-list.addEventListener('click', (e) => {
-  if(e.target.className == 'delete'){
-    const li = e.target.parentElement;
-    li.parentNode.removeChild(li);
-  }
-});
-
-// add books
-const addForm = document.forms['add-book'];
-addForm.addEventListener('submit', function(e){
-  e.preventDefault();
-
-  // create elements
-  const value = addForm.querySelector('input[type="text"]').value;
-  const li = document.createElement('li');
-  const bookName = document.createElement('span');
-  const deleteBtn = document.createElement('span');
-
-  // add text content
-  bookName.textContent = value;
-  deleteBtn.textContent = 'delete';
-
-  // add classes
-  bookName.classList.add('name');
-  deleteBtn.classList.add('delete');
-
-  // append to DOM
-  li.appendChild(bookName);
-  li.appendChild(deleteBtn);
-  list.appendChild(li);
-});
-
-//hidebooks
-const hideBox = document.querySelector('#hide');
-hideBox.addEventListener('change', function(e){
-  if(hideBox.checked){
-    list.style.display = "none";
-  } else {
-    list.style.display = "initial";
-  }
-});
-
 //filter books
 const searchBar = document.forms['search-books'].querySelector('input');
 searchBar.addEventListener('keyup', (e) => {
   const term = e.target.value.toLowerCase();
   const books = list.getElementsByTagName('li');
+  //going through the array of books
   Array.from(books).forEach((book) => {
+    //comparing to see if the word seached matches the words from the array
     const title = book.firstElementChild.textContent;
+    //if it matches displaying it in block and if not displaying none
     if(title.toLowerCase().indexOf(e.target.value) != -1){
       book.style.display = 'block';
     } else {
       book.style.display = 'none';
     }
   });
+});
+
+//tabbed content
+const tabs = document.querySelector('.tabs');
+const panels = document.querySelectorAll('.panel');
+tabs.addEventListener('click', (e) => {
+  if(e.target.tagName == 'LI'){
+    const targetPanel = document.querySelector(e.target.dataset.target);
+    Array.from(panels).forEach((panel) => {
+      if(panel == targetPanel){
+        panel.classList.add('active');
+      }else{
+        panel.classList.remove('active');
+      }
+    });
+  }
 });
