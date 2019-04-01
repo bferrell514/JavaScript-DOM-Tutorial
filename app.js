df277bbffed60512d6b7e944e9a4ff84cffc5122
bferrell514 <bferrell514@gmail.com>
2019-04-01 01:01:55 -0400

    added an add form to stop refreshing deleted books

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ list.addEventListener('click', function(e){
         const li = e.target.parentElement;
         
         //removes the chilf to the parent node
-        list.parentNode.removeChild(li)
+        list.parentNode.removeChild(li);
     }
-})
+});
+
+//add book list
+//stop from reloading the page when you click add 
+const addForm = document.forms['add-book'];
+
+addForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    const value = addForm.querySelector('input[type="text"]');
+    console.log(value);
+});
