6a40024798fa4f6c541752bcf3490fc0ffc98bad
bferrell514 <bferrell514@gmail.com>
2019-04-01 12:41:06 -0400

    adding check box with event listener

--- app.js
+++ app.js
@@ -32,3 +32,14 @@ addForm.addEventListener('submit', function(e){
   li.appendChild(deleteBtn);
   list.appendChild(li);
 });
+
+//hidebooks
+
+const hideBox = document.querySelector('#hide');
+hideBox.addEventListener('change', function(e){
+    if(hideBox.checked) {
+        list.style.display = "none";
+    } else {
+        list.style.display = "intial";
+    }
+});
